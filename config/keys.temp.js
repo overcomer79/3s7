50724a72601e0041f7e12d0e4654678d8423700d
Pierpaolo Monaco <pierpaolo.monaco@gmail.com>
2018-07-09 09:05:47 +0200

    Aggiunto file temporaneo per le chiavi

---
+++ config/keys.temp.js
@@ -0,0 +1,21 @@
+/**
+ * To be modified
+ * Insert the missing data and rename the file to keys.temp
+ */
+
+module.exports = {
+  google: {
+    clientID: "",
+    clientSecret: ""
+  },
+  facebook: {
+    clientID: "",
+    clientSecret: ""
+  },
+  mongoDB: {
+    atlas_conn_string: ""
+  },
+  jwt: {
+    secret: ""
+  }
+};
