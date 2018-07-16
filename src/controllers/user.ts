c610707ccd29baae515d2a88e998969c5cc0b7e1
Pierpaolo Monaco <pierpaolo.monaco@gmail.com>
2018-07-16 16:54:26 +0200

    preimpostato l'utilizzo del middleware per i ruoli

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,11 @@
 import User, { IUser } from "../models/user";
-import * as mongoose from "mongoose";
+// import * as mongoose from "mongoose";
 
 export const users_get_user_by_id = async (req, res, next) => {
-  const foundUser: IUser = await User.findById(req.paramas.userId);
+  const foundUser: IUser = await User.findById(req.params.userId);
+  if (!foundUser) {
+    return res.status(204).send({ error: "user not found" });
+  }
   res.status(200).json({ user: foundUser });
 };
 
